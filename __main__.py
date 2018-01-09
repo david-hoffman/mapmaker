fca720f58818cb2f7ab9ac1b594262dce3e88adb
David Hoffman <dave.p.hoffman@gmail.com>
2018-01-09 14:37:04 -0500

    Save as JPEG with grey colormap so that user can combine channels

--- __main__.py
+++ __main__.py
@@ -16,7 +16,7 @@ import click
 @click.option('--scale', '-s', type=float, default=0.1, help='Scaling for the output images')
 @click.option('--program-type', type=click.Choice(['VSIM', 'SPIM']), default="VSIM", help='Which program took the SIM data.')
 def cli(montage_dir, location_path, scale, program_type):
-    """Read """
+    """Mark imaged locations on montaged widefield data"""
     sim_locations = dict()
     for path in location_path:
         click.echo("Extracting cell locations from {}".format(path))
@@ -24,20 +24,22 @@ def cli(montage_dir, location_path, scale, program_type):
             sim_locations.update(extract_locations_csv(path))
         else:
             sim_locations.update(extract_locations(path))
+    if not len(sim_locations):
+        raise RuntimeError("No locations found.")
     click.echo("Locations: ")
     click.echo("\n".join(["{} @ {:.3f}, {:.3f}".format(k, *v) for k, v in sim_locations.items()]))
 
     for montage_path in montage_dir:
+        montage_shape, tile0_loc = read_montage_settings(glob.glob(montage_path + "3D settings_*.csv")[0])
         click.echo("Reading data from {}".format(montage_path))
         data = load_stack(montage_path)
-        montage_shape, tile0_loc = read_montage_settings(glob.glob(montage_path + "3D settings_*.csv")[0])
         montage_data = montage(data, montage_shape)
         extent = calc_extent(tile0_loc, data.shape[-2:], montage_shape)
-        basename = os.path.dirname(montage_path) + "_ch{}.png"
+        basename = os.path.dirname(montage_path) + "_ch{}.jpg"
         for i, channel in enumerate(montage_data):
             click.echo("Saving {}".format(basename.format(i)))
             make_fig(channel, extent, sim_locations, basename.format(i),
-                     scale, cmap="inferno", gamma=0.25)
+                     scale, cmap="Greys_r", gamma=0.25)
 
 if __name__ == '__main__':
     cli()
